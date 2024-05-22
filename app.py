c9909efffcb9b5d8a3c8798dc46aaca2656f92d3
Oussama <oussamabentaouil1908@gmail.com>
2024-05-22 18:00:50 +0100

    first commit

--- app.py
+++ app.py
@@ -1,50 +1,45 @@
-# Q&A Chatbot
-#from langchain.llms import OpenAI
-
 from dotenv import load_dotenv
-
-load_dotenv()  # take environment variables from .env.
+load_dotenv() ## loading all the environment variables
 
 import streamlit as st
 import os
-import pathlib
-import textwrap
-
 import google.generativeai as genai
 
-from IPython.display import display
-from IPython.display import Markdown
-
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
-os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-## Function to load OpenAI model and get respones
-
+## function to load Gemini Pro model and get responses
+model = genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat(history=[])
 def get_gemini_response(question):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(question)
-    return response.text
-
-##initialize our streamlit app
-
-st.set_page_config(page_title="Q&A Demo")
-
-st.header("Gemini Application")
-
-input=st.text_input("Input: ",key="input")
-
-
-submit=st.button("Ask the question")
-
-## If ask button is clicked
-
-if submit:
     
-    response=get_gemini_response(input)
-    st.subheader("The Response is")
-    st.write(response)
+    response = chat.send_message(question, stream=True)
+    return response
+
+## Initialize Streamlit app
+st.set_page_config(page_title="Chatbot Demo")
+
+st.header("Gemini LLM Application")
+
+# Initialize session state for chat history if it doesn't exist
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+for message  in st.session_state.messages:
+  with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
+if prompt:= st.chat_input("Type your message here:"):
+  st.session_state.messages.append({"role":"user","content":prompt})
+  with st.chat_message("user"):
+    st.markdown(prompt)
+    
+  with st.chat_message("assistant"):
+    message_placeholder = st.empty()
+    full_response = ""
+    for response in get_gemini_response(prompt):
+        full_response += response.text
+        message_placeholder.markdown(full_response)
+        st.session_state.messages.append({"role":"assistant","content":full_response})
+
+
+
